Kick4U2/xmltodict

commit 8374e7d84f9209808373d26a7bbeed3e567ed892

Use Markdown long_description on PyPI (#190)

setup.py
@@ -8,9 +8,15 @@ except ImportError:
 
 import xmltodict
 
+with open('README.md') as f:
+    long_description = f.read()
+
+
 setup(name='xmltodict',
       version=xmltodict.__version__,
       description=xmltodict.__doc__,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author=xmltodict.__author__,
       author_email='martinblech@gmail.com',
       url='https://github.com/martinblech/xmltodict',

commit 375e4cfdf2a323489a745b810c5953fbce403f2c

Update (#1)
* Drop Jython/Python 2 and add Python 3.9/3.10

* Drop OrderedDict in Python >= 3.7

* Fix typo

* Do not use `len()` to determine if a sequence is empty

* Add more namespace attribute tests

* setup: open README.md with a specific encoding

With python3, the built-in open() function opens files in "text" mode by
default (i.e. the f.read() method reads bytes and tries to decode them
to unicode using the interpreter default encoding). Depending on the
system locale, this default encoding may be 'ascii'. Which leads to the
following error:

 ~$ python3 setup.py egg_info
 Traceback (most recent call last):
   File "setup.py", line 12, in <module>
     long_description = f.read()
   File "/usr/lib/python3.6/encodings/ascii.py", line 26, in decode
     return codecs.ascii_decode(input, self.errors)[0]
 UnicodeDecodeError: 'ascii' codec can't decode byte 0xc3 in position
 2587: ordinal not in range(128)

The character at position 2587 is 'ô' (from Fantômas) and is utf-8
encoded.

Force to open the file in "binary" mode (which is the only mode on
python2) and explicitly decode the read bytes to unicode using the
'utf-8' codec.

Fixes: 8374e7d84f92 ("Use Markdown long_description on PyPI (#190)")
Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

* Bump version and update CHANGELOG.

* Update push_release.sh to use twine.

* Fix Travis CI status badge URL

Fixes #296

* fix attributes not appearing in streaming mode

* Add test coverage for #278

* added conda installation command

* Ensure significant whitespace is not trimmed

* Enable `python -m unittest tests/*.py`

Without this, `python -m unittest tests/*.py` would complain
`ModuleNotFoundError: No module named 'tests.test_dicttoxml'`

* Support indent using interger format

Originally `unparse(pretty=True, indent=2)` would fail
with confusing error message,
as it only supported `unparse(pretty=True, indent='  ')`

.travis.yml
@@ -1,26 +1,15 @@
 language: python
 
 python:
-  - "2.7"
   - "3.4"
   - "3.5"
   - "3.6"
   - "3.7"
+  - "3.8"
+  - "3.9"
+  - "3.10-dev"
   - "pypy"
 
-script: nosetests -vv --with-coverage --cover-package=xmltodict
+install: pip install nose2
 
-jobs:
-  include:
-    - python: "3.8"
-      dist: bionic
-    - name: "Jython"
-      python: "pypy"
-      env: JYTHON_VERSION="2.7.0"
-      before_install:
-        - export JYTHON_URL="http://search.maven.org/remotecontent?filepath=org/python/jython-installer/${JYTHON_VERSION}/jython-installer-${JYTHON_VERSION}.jar"
-        - wget $JYTHON_URL -O jython_installer.jar
-        - java -jar jython_installer.jar -s -d $HOME/jython
-        - export PATH="$HOME/jython:$PATH"
-        - $HOME/jython/bin/easy_install nose
-      script: $HOME/jython/bin/nosetests
+script: nose2 -vv --coverage=xmltodict.py

push_release.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-python setup.py clean sdist bdist_wheel upload
+python setup.py clean sdist bdist_wheel
+python -m twine upload dist/*

setup.py
@@ -8,8 +8,8 @@ except ImportError:
 
 import xmltodict
 
-with open('README.md') as f:
-    long_description = f.read()
+with open('README.md', 'rb') as f:
+    long_description = f.read().decode('utf-8')
 
 
 setup(name='xmltodict',
@@ -22,24 +22,23 @@ setup(name='xmltodict',
       url='https://github.com/martinblech/xmltodict',
       license=xmltodict.__license__,
       platforms=['all'],
-      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+      python_requires='>=3.4',
       classifiers=[
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: Implementation :: Jython',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
           'Programming Language :: Python :: Implementation :: PyPy',
           'Topic :: Text Processing :: Markup :: XML',
       ],
       py_modules=['xmltodict'],
-      tests_require=['nose>=1.0', 'coverage'],
+      tests_require=['nose2', 'coverage'],
       )

tox.ini
@@ -1,7 +1,7 @@
 [tox]
-envlist = py27, py34, py35, py36, py37, py38, pypy
+envlist = py34, py35, py36, py37, py38, py39, py310, pypy
 [testenv]
 deps =
   coverage
-  nose
-commands=nosetests --with-coverage --cover-package=xmltodict
+  nose2
+commands=nose2 --coverage=xmltodict.py

xmltodict.py
@@ -15,7 +15,11 @@ except ImportError:  # pragma no cover
     except ImportError:
         from io import StringIO
 
-from collections import OrderedDict
+_dict = dict
+import platform
+if tuple(map(int, platform.python_version_tuple()[:2])) < (3, 7):
+    from collections import OrderedDict as _dict
+
 from inspect import isgenerator
 
 try:  # pragma no cover
@@ -28,7 +32,7 @@ except NameError:  # pragma no cover
     _unicode = str
 
 __author__ = 'Martin Blech'
-__version__ = '0.12.0'
+__version__ = '0.13.0'
 __license__ = 'MIT'
 
 
@@ -46,7 +50,7 @@ class _DictSAXHandler(object):
                  force_cdata=False,
                  cdata_separator='',
                  postprocessor=None,
-                 dict_constructor=OrderedDict,
+                 dict_constructor=_dict,
                  strip_whitespace=True,
                  namespace_separator=':',
                  namespaces=None,
@@ -68,7 +72,7 @@ class _DictSAXHandler(object):
         self.strip_whitespace = strip_whitespace
         self.namespace_separator = namespace_separator
         self.namespaces = namespaces
-        self.namespace_declarations = OrderedDict()
+        self.namespace_declarations = dict_constructor()
         self.force_list = force_list
         self.comment_key = comment_key
 
@@ -101,9 +105,9 @@ class _DictSAXHandler(object):
         attrs = self._attrs_to_dict(attrs)
         if attrs and self.namespace_declarations:
             attrs['xmlns'] = self.namespace_declarations
-            self.namespace_declarations = OrderedDict()
+            self.namespace_declarations = self.dict_constructor()
         self.path.append((name, attrs or None))
-        if len(self.path) > self.item_depth:
+        if len(self.path) >= self.item_depth:
             self.stack.append((self.item, self.data))
             if self.xml_attribs:
                 attr_entries = []
@@ -132,12 +136,12 @@ class _DictSAXHandler(object):
             should_continue = self.item_callback(self.path, item)
             if not should_continue:
                 raise ParsingInterrupted()
-        if len(self.stack):
+        if self.stack:
             data = (None if not self.data
                     else self.cdata_separator.join(self.data))
             item = self.item
             self.item, self.data = self.stack.pop()
-            if self.strip_whitespace and data:
+            if self.strip_whitespace and data and item:
                 data = data.strip() or None
             if data and self.force_cdata and item is None:
                 item = self.dict_constructor()
@@ -254,14 +258,14 @@ def parse(xml_input, encoding=None, expat=expat, process_namespaces=False,
         ...         return key, value
         >>> xmltodict.parse('<a><b>1</b><b>2</b><b>x</b></a>',
         ...                 postprocessor=postprocessor)
-        OrderedDict([(u'a', OrderedDict([(u'b:int', [1, 2]), (u'b', u'x')]))])
+        {'a': {'b:int': [1, 2], 'b': 'x'}}
 
     You can pass an alternate version of `expat` (such as `defusedexpat`) by
     using the `expat` parameter. E.g:
 
         >>> import defusedexpat
         >>> xmltodict.parse('<a>hello</a>', expat=defusedexpat.pyexpat)
-        OrderedDict([(u'a', u'hello')])
+        {'a': 'hello'}
 
     You can use the force_list argument to force lists to be created even
     when there is only a single child of a given level of hierarchy. The
@@ -368,8 +372,8 @@ def parse(xml_input, encoding=None, expat=expat, process_namespaces=False,
         parser.ParseFile(xml_input)
     elif isgenerator(xml_input):
         for chunk in xml_input:
-            parser.Parse(chunk,False)
-        parser.Parse(b'',True)
+            parser.Parse(chunk, False)
+        parser.Parse(b'', True)
     else:
         parser.Parse(xml_input, True)
     return handler.item
@@ -416,7 +420,7 @@ def _emit(key, value, content_handler,
         if full_document and depth == 0 and index > 0:
             raise ValueError('document with multiple roots')
         if v is None:
-            v = OrderedDict()
+            v = _dict()
         elif isinstance(v, bool):
             if v:
                 v = _unicode('true')
@@ -424,13 +428,13 @@ def _emit(key, value, content_handler,
                 v = _unicode('false')
         elif not isinstance(v, dict):
             if expand_iter and hasattr(v, '__iter__') and not isinstance(v, _basestring):
-                v = OrderedDict(((expand_iter, v),))
+                v = _dict(((expand_iter, v),))
             else:
                 v = _unicode(v)
         if isinstance(v, _basestring):
-            v = OrderedDict(((cdata_key, v),))
+            v = _dict(((cdata_key, v),))
         cdata = None
-        attrs = OrderedDict()
+        attrs = _dict()
         children = []
         for ik, iv in v.items():
             if ik == cdata_key:
@@ -449,6 +453,8 @@ def _emit(key, value, content_handler,
                 attrs[ik[len(attr_prefix):]] = iv
                 continue
             children.append((ik, iv))
+        if type(indent) is int:
+            indent = ' ' * indent
         if pretty:
             content_handler.ignorableWhitespace(depth * indent)
         content_handler.startElement(key, AttributesImpl(attrs))

tests/test_dicttoxml.py
@@ -144,6 +144,26 @@ class DictToXMLTestCase(unittest.TestCase):
         self.assertEqual(xml, unparse(obj, pretty=True,
                                       newl=newl, indent=indent))
 
+    def test_pretty_print_with_int_indent(self):
+        obj = {'a': OrderedDict((
+            ('b', [{'c': [1, 2]}, 3]),
+            ('x', 'y'),
+        ))}
+        newl = '\n'
+        indent = 2
+        xml = dedent('''\
+        <?xml version="1.0" encoding="utf-8"?>
+        <a>
+          <b>
+            <c>1</c>
+            <c>2</c>
+          </b>
+          <b>3</b>
+          <x>y</x>
+        </a>''')
+        self.assertEqual(xml, unparse(obj, pretty=True,
+                                      newl=newl, indent=indent))
+
     def test_encoding(self):
         try:
             value = unichr(39321)

tests/test_xmltodict.py
@@ -93,13 +93,13 @@ class XMLToDictTestCase(unittest.TestCase):
         """
         self.assertEqual(
             parse(xml),
-            {'root': {'emptya': None,
+            {'root': {'emptya': "           ",
                       'emptyb': {'@attr': 'attrvalue'},
                       'value': 'hello'}})
 
     def test_keep_whitespace(self):
         xml = "<root> </root>"
-        self.assertEqual(parse(xml), dict(root=None))
+        self.assertEqual(parse(xml), dict(root=' '))
         self.assertEqual(parse(xml, strip_whitespace=False),
                          dict(root=' '))
 
@@ -183,7 +183,8 @@ class XMLToDictTestCase(unittest.TestCase):
         xml = """
         <root xmlns="http://defaultns.com/"
               xmlns:a="http://a.com/"
-              xmlns:b="http://b.com/">
+              xmlns:b="http://b.com/"
+              version="1.00">
           <x a:attr="val">1</x>
           <a:y>2</a:y>
           <b:z>3</b:z>
@@ -191,12 +192,13 @@ class XMLToDictTestCase(unittest.TestCase):
         """
         d = {
             'http://defaultns.com/:root': {
+                '@version': '1.00',
+                '@xmlns': {
+                    '': 'http://defaultns.com/',
+                    'a': 'http://a.com/',
+                    'b': 'http://b.com/',
+                },
                 'http://defaultns.com/:x': {
-                    '@xmlns': {
-                        '': 'http://defaultns.com/',
-                        'a': 'http://a.com/',
-                        'b': 'http://b.com/',
-                    },
                     '@http://a.com/:attr': 'val',
                     '#text': '1',
                 },
@@ -211,7 +213,8 @@ class XMLToDictTestCase(unittest.TestCase):
         xml = """
         <root xmlns="http://defaultns.com/"
               xmlns:a="http://a.com/"
-              xmlns:b="http://b.com/">
+              xmlns:b="http://b.com/"
+              version="1.00">
           <x a:attr="val">1</x>
           <a:y>2</a:y>
           <b:z>3</b:z>
@@ -223,12 +226,13 @@ class XMLToDictTestCase(unittest.TestCase):
         }
         d = {
             'root': {
+                '@version': '1.00',
+                '@xmlns': {
+                    '': 'http://defaultns.com/',
+                    'a': 'http://a.com/',
+                    'b': 'http://b.com/',
+                },
                 'x': {
-                    '@xmlns': {
-                        '': 'http://defaultns.com/',
-                        'a': 'http://a.com/',
-                        'b': 'http://b.com/',
-                    },
                     '@ns_a:attr': 'val',
                     '#text': '1',
                 },
@@ -243,7 +247,8 @@ class XMLToDictTestCase(unittest.TestCase):
         xml = """
         <root xmlns="http://defaultns.com/"
               xmlns:a="http://a.com/"
-              xmlns:b="http://b.com/">
+              xmlns:b="http://b.com/"
+              version="1.00">
           <x a:attr="val">1</x>
           <a:y>2</a:y>
           <b:z>3</b:z>
@@ -252,12 +257,13 @@ class XMLToDictTestCase(unittest.TestCase):
         namespaces = collections.defaultdict(lambda: None)
         d = {
             'root': {
+                '@version': '1.00',
+                '@xmlns': {
+                    '': 'http://defaultns.com/',
+                    'a': 'http://a.com/',
+                    'b': 'http://b.com/',
+                },
                 'x': {
-                    '@xmlns': {
-                        '': 'http://defaultns.com/',
-                        'a': 'http://a.com/',
-                        'b': 'http://b.com/',
-                    },
                     '@attr': 'val',
                     '#text': '1',
                 },
@@ -272,7 +278,8 @@ class XMLToDictTestCase(unittest.TestCase):
         xml = """
         <root xmlns="http://defaultns.com/"
               xmlns:a="http://a.com/"
-              xmlns:b="http://b.com/">
+              xmlns:b="http://b.com/"
+              version="1.00">
           <x>1</x>
           <a:y>2</a:y>
           <b:z>3</b:z>
@@ -283,6 +290,7 @@ class XMLToDictTestCase(unittest.TestCase):
                 '@xmlns': 'http://defaultns.com/',
                 '@xmlns:a': 'http://a.com/',
                 '@xmlns:b': 'http://b.com/',
+                '@version': '1.00',
                 'x': '1',
                 'a:y': '2',
                 'b:z': '3',
@@ -449,3 +457,21 @@ class XMLToDictTestCase(unittest.TestCase):
             }
         }
         self.assertEqual(parse(xml, process_comments=True), expectedResult)
+
+    def test_streaming_attrs(self):
+        xml = """
+        <a>
+            <b attr1="value">
+                <c>cdata</c>
+            </b>
+        </a>
+        """
+        def handler(path, item):
+            expected = {
+                '@attr1': 'value',
+                'c': 'cdata'
+            }
+            self.assertEqual(expected, item)
+            return True
+
+        parse(xml, item_depth=2, item_callback=handler)